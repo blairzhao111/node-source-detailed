1a4ea8eefb3a802534a42769826df0f6b301043a
Junwei Zhao <blairzhao111@gmail.com>
2016-08-21 22:30:24 -0400

    update module.js

--- module.js
+++ module.js
@@ -1,3 +1,48 @@
+// 'module' module
+
+//if you feel like that the source code is TL;DR, then here is the deal:
+
+// node.js module system follows CommonJS standard and specification.
+// Which every script is considered as an individual module and exposes public objects/values through an export property of that module's module object. 
+// All other stuff declared inside that module but not marked as export are considered to be private to the module.
+
+// If you want to load some certain module, use global(more likely is in module-scope) 'require' method(it's in Module.prototype object) of a Module instance(which is the module you are working with)
+// Once the module has initially been loaded, it'll get cached inside . So next time when you require it again. The same module instance would be returned. 
+
+// The entire script/module will be executed and loaded in a Native Module Wrapper Function, this wrapper function gets invoked with five arguments
+// They are following:
+// 1. exports: the references to initial module.exports object, if you explicitly replace module.exports, then this reference is not valid anymore.
+// 2. require: a wrapper function object that internally invokes module.require method for loading requested module. And it provides a proxy as require.cache to expose accessing to Module._cache object.
+// 3. module: the module object of the module script that is going to be loaded in environment.
+// 4. __filename: a variable that stores the absolute path of current file in your local file system. 
+// 5. __dirname: a variable that stores the absolute path of directory that the file's located in your local file system. 
+
+// Everytime when you use 'require' method and try to load a given module, node will try to check if there's any matches in a series of directories in cached paths.
+// when require(X) from module at path Y
+// 1. If X is a core module,
+//    a. return the core module
+//    b. STOP
+// 2. If X begins with './' or '/' or '../'
+//    a. LOAD_AS_FILE(Y + X)
+//    b. LOAD_AS_DIRECTORY(Y + X)
+// 3. LOAD_NODE_MODULES(X, dirname(Y))
+// 4. THROW "not found"
+
+// LOAD_AS_FILE(X)
+// 1. If X is a file, load X as JavaScript text.  STOP
+// 2. If X.js is a file, load X.js as JavaScript text.  STOP
+// 3. If X.json is a file, parse X.json to a JavaScript Object.  STOP
+// 4. If X.node is a file, load X.node as binary addon.  STOP
+
+// LOAD_AS_DIRECTORY(X)
+// 1. If X/package.json is a file,
+//    a. Parse X/package.json, and look for "main" field.
+//    b. let M = X + (json main field)
+//    c. LOAD_AS_FILE(M)
+// 2. If X/index.js is a file, load X/index.js as JavaScript text.  STOP
+// 3. If X/index.json is a file, parse X/index.json to a JavaScript object. STOP
+// 4. If X/index.node is a file, load X/index.node as binary addon.  STOP
+
 'use strict';
 
 const NativeModule = require('native_module');
@@ -12,9 +57,14 @@ const internalModuleReadFile = process.binding('fs').internalModuleReadFile;
 const internalModuleStat = process.binding('fs').internalModuleStat;
 const preserveSymlinks = !!process.binding('config').preserveSymlinks;
 
+
 // A helper function for isolating hasOwnProperty from Object.prototype
 // If obj.hasOwnProperty has been overridden, then calling
 // obj.hasOwnProperty(prop) will break.
+
+// A common practice is not directly using any methods inherited from Object.prototype
+// There are some small chances that they would be overriden.
+// Best way is to use them in call/apply form or wrap them as bound functions.
 function hasOwnProperty(obj, prop) {
 	return Object.prototype.hasOwnProperty.call(obj, prop);
 }
@@ -36,12 +86,13 @@ stat.cache = null;
 
 // Module Constructor
 // Evert module instance has following properties:
-// 1. id: denoted by script's absolute path(filename) 
-// 2. exports: an object stores all module exports, could be overridden to any value
-// 3. parent: refernce to the module that loads current one
-// 4. filename: an absolute path of module file/script in file system
-// 5. loaded: a flag indicates whether current module is loaded from some other module
+// 1. id: denoted by script's absolute path(filename), the main module would be set to '.'
+// 2. exports: an object stores all module's exporting values, could be overridden to any value, like another object/function/any primitive values.
+// 3. parent: refernce to the parent module that loads the current one
+// 4. filename: an absolute path of current module file/script in file system
+// 5. loaded: a flag indicates whether current module is loaded by some other module
 // 6. children: an array of modules that get loaded by the current module
+// 7. paths: an array of paths for modules lookup.
 function Module(id, parent) {
 	this.id = id;
 	this.exports = {};
@@ -55,22 +106,197 @@ function Module(id, parent) {
 	this.children = [];
 }
 
+//entire script exports this Module constructor
 module.exports = Module;
 
-//object used to cache all loaded modules objects, modules are cached as filename/module obj pairs.
+//properties of Module constructor function object:
+
+// Object used to cache all loaded modules objects, modules are cached as filename/module object pairs.
+// If the module gets required first time, it would be loaded and run, and then a module object gets created and stored in this Module._cache object.
+// When next time this module gets required, node would return the export property of the cached module object in Module._cache instead of reloading it.
+// This's like the Singleton Pattern, only one module object exists all the time except you do something.
+// If you need to load a brand new module object whenever you require it. You need to explicitly delete the previous cached one before you require the module.
 Module._cache = {};
-//object used to
+
+//object used to cache all paths that store module
 Module._pathCache = {};
-//object used to store all valid extensions
+
+//object used to store all valid extensions, by default, there are three: '.js'/'.json'/'.node'
 Module._extensions = {};
+
 var modulePaths = [];
 Module.globalPaths = [];
 
+//
 Module.wrapper = NativeModule.wrapper;
 Module.wrap = NativeModule.wrap;
 Module._debug = util.debuglog('module');
 
+// We use this alias for the preprocessor that filters it out
 const debug = Module._debug;
+
+// given a module name, and a list of paths to test, returns the first
+// matching file in the following precedence.
+//
+// require("a.<ext>")
+//   -> a.<ext>
+//
+// require("a")
+//   -> a
+//   -> a.<ext>
+//   -> a/index.<ext>
+
+//check if the directory is a package.json directory
+const packageMainCache = {};
+
+//what this function does is bascially go and try to locate the package.json file, if it exists, then return its 'main' property value.
+function readPackage(requestPath) {
+	//if it's already got cached, simply return it from the cache.
+	if(hasOwnProperty(packageMainCache, requestPath)){
+		return packageMainCache[requestPath];
+	}
+
+	//builds path to package.json file in the directroy and try to read that package.json file into memory
+	const jsonPath = path.resolve(requestPath, 'package.json');
+	const json = internalModuleReadFile(path._makeLong(jsonPath));
+
+	//no package.json is found in the directory, simply return false
+	if(json === undefined){
+		return false;
+	}
+
+	//if there's a package.json, then try to parse it and return its main property, which indicates the main entry file of entire module.
+	try {
+		var pkg = packageMainCache[requestPath] = JSON.parse(json).main;
+	} catch(e) {
+		e.path = jsonPath;
+		e.message = 'Error parsing ' + jsonPath + ': ' + e.message;
+    	throw e;
+	}
+
+	//if all okay, then return the value of that 'main' property in package.json file, which indicates the 
+	return pkg;
+}
+
+//what this function does is to try to get the entry file name, then try to load that file without/with extensions.
+function tryPackage(requestPath, exts, isMain) {
+	var pkg = readPackage(requestPath);
+
+	if(!pkg){
+		return false;
+	}
+
+	var filename = path.resolve(requestPath, pkg);
+
+	return tryFile(filename, isMain) || tryExtensions(filename, exts, isMain) || tryExtensions(path.resolve(filename, 'index'), exts, isMain);
+}
+
+// check if the file exists and is not a directory
+// if using --preserve-symlinks and isMain is false,
+// keep symlinks intact, otherwise resolve to the
+// absolute realpath.
+function tryFile(requestPath, isMain) {
+	const rc = stat(requestPath);
+
+	if(preserveSymlinks && !isMain){
+		return rc === 0 && path.resolve(requestPath);
+	}
+
+	return rc === 0 && fs.realpathSync(requestPath);
+}
+
+// given a path check a the file exists with any of the set extensions
+function tryExtensions(p, exts, isMain) {
+	//try all extensions in extension pool, normally including '.js'/'.json'/'.node'
+	for(var i = 0; i < exts.length; i++) {
+		const filename = tryFile(p + exts[i], isMain);
+
+		if(filename){
+			return filename;
+		}
+	}
+
+	return false;
+}
+
+var warned = false;
+Module._findPath = function(request, paths, isMain) {
+	if(path.isAbsolute(request)){
+		paths = [''];
+	} else if(!paths || paths.length === 0) {
+		return false;
+	}
+
+	const cacheKey = JSON.stringify({request: request, paths: paths});
+	if(Module._pathCache[cacheKey]){
+		return Module._pathCache[cacheKey];
+	}
+
+	var exts;
+	const trailingSlash = request.length > 0 && request.charCodeAt(request.length - 1) === 47; /*/*/
+
+	//For each path
+	for(var i = 0; i < paths.length; i++){
+		// Don't search further if path doesn't exist
+		const currentPath = paths[i];
+
+		if(currentPath && stat(currentPath) < 1) continue;
+
+		var basePath = path.resolve(currentPath, request);
+		var filename;
+
+		if(!trailingSlash){
+			const rc = stat(basePath);
+			if(rc === 0){ // File
+				if(preserveSymlinks && !isMain){
+					filename = path.resolve(basePath);
+				} else {
+					filename = fs.realpathSync(basePath);
+				}
+			} else if(rc === 1) { // Directory
+				if(exts === undefined){
+					exts = Object.keys(Module._extensions);
+					filename = tryPackage(basePath, exts, isMain);
+				}
+			}
+
+			if(!filename){
+				//try it with each of the extensions
+				if(exts === undefined){
+					exts = Object.keys(Module._extensions);
+					filename = tryExtensions(basePath, exts, isMain);
+				}
+			}
+		}
+
+	    if (!filename) {
+	      if (exts === undefined)
+	        exts = Object.keys(Module._extensions);
+	      filename = tryPackage(basePath, exts, isMain);
+	    }
+
+	    if (!filename) {
+	      // try it with each of the extensions at "index"
+	      if (exts === undefined)
+	        exts = Object.keys(Module._extensions);
+	      filename = tryExtensions(path.resolve(basePath, 'index'), exts, isMain);
+	    }
+
+	    if (filename) {
+	      // Warn once if '.' resolved outside the module dir
+	      if (request === '.' && i > 0) {
+	        warned = internalUtil.printDeprecationMessage(
+	          'warning: require(\'.\') resolved outside the package ' +
+	          'directory. This functionality is deprecated and will be removed ' +
+	          'soon.', warned);
+	    }
+
+	    Module._pathCache[cacheKey] = filename;
+	    return filename;
+	}
+
+	return false;
+};
 
 // Check the cache for the requested file.
 // 1. If a module already exists in the cache: return its exports object.
@@ -205,14 +431,9 @@ Module._extensions['.node'] = function(module, filename) {
 	return process.dlopen(module, path._makeLong(filename));
 };
 
-//method from loading the main module from the command line and start executions
-Module.runMain = function() {
-	// load the main module from command line argument
-	// pass filename, set parent reference to null and mark isMain to true
-	Module._load(process.argv[1], null, true);
-	// Handle any nextTicks added in the first tick of the program
-	process._tickCallback();
-}
+// Resolved path to process.argv[1] will be lazily placed here
+// (needed for setting breakpoint when called with --debug-brk)
+var resolvedArgv;
 
 // Run the file contents in the correct scope or sandbox.
 // Expose the correct helper variables (require, module, exports) to the file
@@ -244,7 +465,125 @@ Module.prototype._compile = function(content, filename) {
 			}
 		}
 	}
+
+	// create wrapper function
+	var wrapper = Module.wrap(content);
+
+	//use 'v8' to create a compiled wrapper
+	var compiledWrapper = vm.runInThisContext(wrapper, {
+		filename: filename,
+		lineOffset: 0,
+		displayErrors: true
+	});
+
+	if(process._debugWaitConnect){
+		if(!resolvedArgv){
+			// we enter the repl if we're not given a filename argument.
+			if (process.argv[1]) {
+			resolvedArgv = Module._resolveFilename(process.argv[1], null);
+			} else {
+			resolvedArgv = 'repl';
+			}			
+		}
+
+		// Set breakpoint on module start
+		if(filename === resolvedArgv){
+			delete process._debugWaitConnect;
+			const Debug = vm.runInThisContext('Debug');
+			Debug.setBreakPoint(compiledWrapper, 0, 0);
+		}
+	}
+
+	var dirname = path.dirname(filename);
+
+	var require = internalModule.makeRequestFunction.call(this);
+
+	//for every module, there are five arguments that get exposed to wrapper function.
+	var args = [this.exports, require, this, filename, dirname];
+
+	var depth = internalModule.requireDepth;
+	if(depth === 0)	stat.cache = new Map();
+	var result = compiledWrapper.apply(this.exports, args);
+	if(depth === 0) stat.cache = null;
+	return result;
 };
+
+
+//method from loading the main module from the command line and start executions
+Module.runMain = function() {
+	// load the main module from command line argument
+	// pass filename, set parent reference to null and mark isMain to true
+	Module._load(process.argv[1], null, true);
+	// Handle any nextTicks added in the first tick of the program
+	process._tickCallback();
+}
+
+//add some initial paths into node module search path options
+Module._initPaths = function() {
+	//check if running environment's OS is windows
+	const isWindows = process.platform === 'win32';
+
+	//get user's home directory
+	//in window platform, this could be accessed through environment variable, USERPROFILE
+	//in unix-like platform, this could be accessed through environment variable, HOME
+	var homeDir;
+	if(isWindows){
+		homeDir = process.env.USERPROFILE;
+	} else {
+		homeDir = process.env.HOME;
+	}
+
+
+	var paths = [path.resolve(process.execPath, '..', '..', 'lib', 'node')];
+
+	//add paths to homeDir/.node_libraries and homeDir/.node_modules into search path array.
+	if(homeDir){
+		paths.unshift(path.resolve(homeDir, '.node_libraries'));
+		paths.unshift(path.resolve(homeDir, '.node_modules'));
+	}
+
+	//split the node path env variable and add all valid paths into search path array.
+	var nodePath = process.env['NODE_PATH'];
+	if(nodePath){
+		paths = nodePath.split(path.delimiter).filter(function(path){
+			return !!path;
+		}).concat(paths);
+	}
+
+	//set modulePaths variable to search path array
+	modulePaths = paths;
+
+
+  	// clone as a read-only copy, for introspection.
+  	Module.globalPaths = modulePaths.slice(0);
+};
+
+Module._preloadModules = function(requests) {
+	if(!Array.isArray(requests)){
+		return;
+	}
+
+	// Preloaded modules have a dummy parent module which is deemed to exist
+	// in the current working directory. This seeds the search path for
+	// preloaded modules.
+	var parent = new Module('internal/preload', null);
+
+	try {
+		parent.paths = Module._nodeModulePaths(process.cwd());
+	} cache(e) {
+		if(e.code !== 'ENOENT'){
+			throw e;
+		}
+	}
+
+	//require modules in requested list one by one
+	requests.forEach(function(request) {
+		parent.require(request);
+	});
+};
+
+//invoke _initPaths to load all initial search paths into module system
+Module._initPaths();
 
 // backwards compatibility
 Module.Module = Module;
