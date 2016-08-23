a439dd1eb22e0c4db32d99ae2add8806df0c277d
Junwei Zhao <blairzhao111@gmail.com>
2016-08-23 19:14:49 -0400

    update 'http.js' module

--- http.js
+++ http.js
@@ -1,28 +1,54 @@
-//"http" module
+// "http" module
+// "http" module contains all related functionalities of http client/server.
+// Similar to "Stream" module, "http" module itself doesn't provide many inplementations. 
+// It's more like a general API interface that gathers and exposes multiple submodule.
+
+// "http" module provides five subtypes, they are following:
+//   1. http.Server, an instance of http.Server represents a http server.
+//   2. http.ClientRequest, an instnce of http.ClientRequest represents an in-progress request from client to server. 
+//   3. http.IncomingMessage, an instance of http.IncomingMessage represents incoming message to the dest. 
+//   4. http.ServerResponse, an instance of http.ServerResponse represents the http response in the server side.
+//	 5. http.Agent, an instance of http.Agent is used for pooling sockets used in HTTP client requests.
+
+// "http" module contains functionalities for two roles: http Client and http Server
+// For Client part:
+// 	 1. A http request issued from client to server is an instance of http.ClientRequest, which is implemented as Writable stream.
+//   2. Normally http client request is sent by calling http.request/http.get and passing along with necessary server and protocol information.
+//   3. When response for previous client request is returned from server, a 'response' event is emitted and response listener is invoked.
+//   4. When client's response listener is invoked, 'response' object is passed as argument to that handler function.
+//   5. The 'response' object is an instance of http.IncomingMessage. 
+// For Server part:
+//   1. A http server is an instance of http.Server class, which is inherited from net.Server
+//   2. Normally http server is created and returned by calling http.createServer() and passing along with a requestListener function for 'request' event.
+//   3. Everytime when a new http request is coming in, a 'request' is emitted and that requestListener would be invoked to habdle the request.
+//   4. When new request coming in, requestListener is invoked with two objects, 'request' object and 'response' object.
+//   5. The 'request' object is an instance of http.incomingMessage.
+//   6. The 'response' object is an instance of http.serverResponse.
 
 'use strict';
 
 const util = require('util');
 const internalUtil = require('internal/util');
 const EventEmitter = require('events');
-
-exports.IncomingMessage = require('_http_incoming').IncomingMessage;
-
 const common = require('_http_common');
 
-//Expose a list of the HTTP methods that are supported by the parser.
+//Expose a list of http methods that are supported by the parser.
 exports.METHODS = common.methods.slice().sort();
 
 exports.OutgoingMessage = require('_http_outgoing').OutgoingMessage;
 
-//Expose a collection of all the standard HTTP response status codes, and the short description of each.
-exports.STATUS_CODES = server.STATUS_CODES;
+
+// ==============================================================================================================
+// http.Agent
 
 const agent = require('_http_agent');
 const Agent = exports.Agent = agent.Agent;
 
 //Global instance of Agent which is used as the default for all http client requests.
 exports.globalAgent = agent.globalAgent;
+
+// ==============================================================================================================
+// http.ClientRequest
 
 //Expose the ClientRequest class. An instance of ClientRequest is created internally and returned from http.request().
 //A ClientRequest object represents an in-progress request whose header has already been queued. 
@@ -61,20 +87,37 @@ exports.get = function(options, cb) {
 	return req;
 };
 
-//This class inherits from net.Server and represents a HTTP server.
+// ============================================================================================================
+// http.Server Section:
+
 const server = require('_http_server');
-//This object is created internally by a HTTP server, not by the user. It is passed as the second parameter to the 'request' event.
-//The response implements, but does not inherit from, the Writable Stream interface. 
-exports.ServerResponse = server.ServerResponse;
+
+// exports the Server type as http.Server
+const Server = exports.Server = server.Server;
 
 exports._connectionListener = server._connectionListener;
 
-//expose the Server class as http.Server
-const Server = exports.Server = server.Server;
+//Exports a collection of all standard HTTP response status codes, along with short description of each.
+exports.STATUS_CODES = server.STATUS_CODES;
 
-//return a new instance of http.Server, representing a http server.
-//requestListener is a function like function(request, response){}
-//requestListener is added to the 'request' event, this event would emit when a new request coming in.
+// invoking this function returns a new instance of http.Server, which represents a http server.
+// requestListener is a listener function listening on server 'request' event,, this event would emit when a new request coming in.
+// requestListener gets invoked with two arguments, like function(request, response){}
+// request object could be used to extract incoming http request's information
+// response object could be used to send proper server response back to request issuer.
 exports.createServer = function(requestListener) {
 	return new Server(requestListener);
 };
+
+// ============================================================================================================
+// http.ServerResponse Section:
+
+// The instance of this type is created internally by HTTP servers, not by the users. 
+// Instance is created and passed as the second parameter 'response' object to requestListener function that listens on 'request' event.
+// The ServerResponse implements, but does not inherit from, the Writable Stream interface. 
+exports.ServerResponse = server.ServerResponse;
+
+// =============================================================================================================
+// http.IncomingMessage Section:
+
+exports.IncomingMessage = require('_http_incoming').IncomingMessage;
